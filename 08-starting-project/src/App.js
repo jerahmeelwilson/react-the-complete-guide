3dd60ec6021ef03f995912153ff6789906215843
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2023-03-31 00:18:51 -0500

    Completed section 8

---
+++ 08-starting-project/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, userAge) => {
+    setUsersList((prevState) => [
+      ...prevState,
+      { name: username, age: userAge, id: Math.random().toString() },
+    ]);
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
